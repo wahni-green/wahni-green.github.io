8b0046d8ac39725502c5be3d8df6848d46c64f64
Dany Robert <danyrt@wahni.com>
2023-09-03 14:09:26 +0530

    fix: clean up and youtube api

--- frappeverse.js
+++ frappeverse.js
@@ -8,3 +8,36 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+// YT - Video
+let tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+let firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+let player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '720',
+        width: '1280',
+        videoId: 'g12bKzfuUsw',
+        playerVars: {
+            'playsinline': 1,
+            // 'autoplay': 1,
+        },
+        events: {
+            'onReady': onPlayerReady,
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.setSize(window.innerWidth*9/10, window.innerHeight*9/10);
+    event.target.mute();
+    event.target.setPlaybackRate(1.5);
+    event.target.setPlaybackQuality("hd720");
+    event.target.seekTo(20, true);
+    event.target.pauseVideo();
+    // event.target.playVideo();
+}
